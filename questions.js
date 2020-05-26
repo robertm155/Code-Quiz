67e886c038944439ecf7a6defeed9b761b606a0f
Rob Mason <robertddmason@gmail.com>
2020-05-25 21:31:35 -0700

    Added questions to javascript

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "The condition in an if/else statement is enclosed within ____.",
+      choices: ["A. Quotes", "B. Curly Brackets", "C. Parentheses", "D. Square Brackets"],
+      answer: "A"
+    },
+    {
+      title: "What is NOT one of the capitals of South Africa?",
+      choices: ["A. Pretoria", "B. Cape Town", "C. Blomenfontain", "D. Johannesburg"],
+      answer: "D"
+    },
+    {
+      title: "Which movie won the Academy Award for Best Picture in 1993?",
+      choices: ["A. The Fugitive", "B. Silence of the Lambs", "C. Schindler's List", "D. Unforgiven"],
+      answer: "C"
+    },
+    {
+      title: "What is the boiling point of water in fahrenheit?",
+      choices: ["A. 244°F", "B. 315°F", "C. 205°F", "D. 211°F"],
+      answer: "D"
+    },
+    {
+      title: "Which river is the longest in the World?",
+      choices: ["A. The Mississippi", "B. The Nile", "C. The Amazon", "D. The Yangtze"],
+      answer: "B"
+    }
+    
+  ];
